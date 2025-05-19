48bd10c51f11f53467b5aab1ec6b37fab64a20f0
Controle de Gastos <rod.frnnds@gmail.com>
2025-05-19 19:35:34 -0300

    AccountBank

--- src/model/Exception/DomainException.java
+++ src/model/Exception/DomainException.java
@@ -1,9 +1,9 @@
 package model.Exception;
 
 import java.io.Serial;
-import java.io.Serializable;
 
-public class DomainException extends RuntimeException implements Serializable {
+
+public class DomainException extends RuntimeException {
     @Serial
     private static final long serialVersionUID = 1L;
 
